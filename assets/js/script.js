c9456479e3b594899cdde2d3471bd9a64fbc50dc
DelphineLecorney <Lecorney.Delphine@gmail.com>
2023-05-02 16:02:37 +0200

    Add score

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,12 +2,24 @@ const container = document.createElement("div");
 container.classList.add("container");
 document.body.appendChild(container);
 
+const scoreElement = document.createElement("div");
+scoreElement.innerText = "Score: 0";
+scoreElement.classList.add("score");
+document.body.appendChild(scoreElement);
+
+let score = 0;
+
 for (let i = 0; i < 12; i++) {
   const cercle = document.createElement("div");
   cercle.classList.add("cercle");
   container.appendChild(cercle);
+  cercle.addEventListener("click", (event) => {
+    if (event.target.style.backgroundColor === "red") {
+      score++;
+      scoreElement.innerText = `Score: ${score}`;
+    }
+  });
 }
-
 
 let nbCercle = 12;
 let index = 0;
@@ -21,4 +33,4 @@ function displayMoles() {
   const randomIndex = Math.floor(Math.random() * nbCercle);
   cercles[randomIndex].style.backgroundColor = "red";
 }
-setInterval(displayMoles, 1800);
+setInterval(displayMoles, 1300);
