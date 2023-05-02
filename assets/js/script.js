3995b5c249d14f1ef7d016340bff03485cb1a624
DelphineLecorney <Lecorney.Delphine@gmail.com>
2023-05-02 15:17:21 +0200

    Add random on file js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,24 @@
-
-const container = document.createElement('div');
-container.classList.add('container');
+const container = document.createElement("div");
+container.classList.add("container");
 document.body.appendChild(container);
 
 for (let i = 0; i < 12; i++) {
-  const cercle = document.createElement('div');
-  cercle.classList.add('cercle');
+  const cercle = document.createElement("div");
+  cercle.classList.add("cercle");
   container.appendChild(cercle);
 }
+
+
+let nbCercle = 12;
+let index = 0;
+
+function displayMoles() {
+  const cercles = container.querySelectorAll(".cercle");
+  cercles.forEach((cercle) => {
+    cercle.style.backgroundColor = "white";
+  });
+
+  const randomIndex = Math.floor(Math.random() * nbCercle);
+  cercles[randomIndex].style.backgroundColor = "red";
+}
+setInterval(displayMoles, 1800);
